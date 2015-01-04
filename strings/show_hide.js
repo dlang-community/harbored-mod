ddfa6b4f3a0bd4ea64562523d8be133c356e6379
Akzhigitov Anton <akzhigitov.anton@yandex.ru>
2015-01-04 16:57:51 +0300

    work with disabled javascript

--- strings/show_hide.js
+++ strings/show_hide.js
@@ -1,3 +1,11 @@
+window.onload = function(e)
+{
+    console.log( "onload" );
+    var elems = document.querySelectorAll( "div.toc ul ul" );
+    for( i in elems )
+        elems[i].style.display = "none";
+}
+
 function show_hide(id) 
 { 
     var elem = document.getElementById( id ); 
